94163b58280e26d8601fe07690728e35a141cf4e
TheRoundRoads <michaeljosephgoh@gmail.com>
2023-02-09 22:05:51 +0800

    styling and login also incorporate farms

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,30 @@
 import flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = flask.Flask(__name__)
+farm_list = []
 
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/farms/', methods=['GET', 'POST'])
+def farms():
+    if request.method == 'POST':
+        # Add the new farm's information to the list
+        farm = {
+            'name': request.form['name'],
+            'location': request.form['location'],
+            'produce': request.form['produce'],
+            'methods': request.form['methods'],
+            'process': request.form['process']
+        }
+        farm_list.append(farm)
+        return redirect('/farms/')
+    else:
+        return render_template('farms.html', farms=farm_list)
+
+@app.route('/home/')
 def home():
     return render_template("home.html")
 
