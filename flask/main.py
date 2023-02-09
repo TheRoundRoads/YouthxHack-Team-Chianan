f363048c368c14c6400113141091f4b32b31cd33
TheRoundRoads <michaeljosephgoh@gmail.com>
2023-02-09 16:43:48 +0800

    added timelines

--- flask/main.py
+++ flask/main.py
@@ -7,17 +7,17 @@ app = flask.Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/cucumber/')
-def cucumber():
-    return render_template("cucumber.html")
-
 @app.route('/eggs/')
 def eggs():
     return render_template("eggs.html")
 
-@app.route('/chicken/')
-def chicken():
-    return render_template("chicken.html")
+@app.route('/mushroom/')
+def mushroom():
+    return render_template("mushroom.html")
+
+@app.route('/barramundi/')
+def barramundi():
+    return render_template("barramundi.html")
 
 if __name__ == '__main__':
     app.run()
